30179dfba1eb7046dfe1e263a9b1cb38c0304ee5
ChazPollino <132384915+ChazPollino@users.noreply.github.com>
2023-06-09 00:38:58 -0400

    Add files via upload

---
+++ Activity 13/Activity 13/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Activity_13
+{
+    partial class Activity13
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.TopLeftButton = new System.Windows.Forms.Button();
+            this.TopMiddleButton = new System.Windows.Forms.Button();
+            this.TopRightButton = new System.Windows.Forms.Button();
+            this.MiddleLeftButton = new System.Windows.Forms.Button();
+            this.MiddleButton = new System.Windows.Forms.Button();
+            this.MiddleRightButton = new System.Windows.Forms.Button();
+            this.BottomLeftButton = new System.Windows.Forms.Button();
+            this.BottomMiddleButton = new System.Windows.Forms.Button();
+            this.BottomRightButton = new System.Windows.Forms.Button();
+            this.PlayAgainButton = new System.Windows.Forms.Button();
+            this.TurnLabel = new System.Windows.Forms.Label();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.WinnerLabel = new System.Windows.Forms.Label();
+            this.Description2Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.TitleLabel.Location = new System.Drawing.Point(347, 28);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(834, 83);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Lets Play Tic Tac Toe";
+            this.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TopLeftButton
+            // 
+            this.TopLeftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.TopLeftButton.Location = new System.Drawing.Point(572, 181);
+            this.TopLeftButton.Name = "TopLeftButton";
+            this.TopLeftButton.Size = new System.Drawing.Size(165, 153);
+            this.TopLeftButton.TabIndex = 1;
+            this.TopLeftButton.UseVisualStyleBackColor = true;
+            this.TopLeftButton.Click += new System.EventHandler(this.TopLeftButton_Click);
+            // 
+            // TopMiddleButton
+            // 
+            this.TopMiddleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.TopMiddleButton.Location = new System.Drawing.Point(743, 181);
+            this.TopMiddleButton.Name = "TopMiddleButton";
+            this.TopMiddleButton.Size = new System.Drawing.Size(165, 153);
+            this.TopMiddleButton.TabIndex = 2;
+            this.TopMiddleButton.UseVisualStyleBackColor = true;
+            this.TopMiddleButton.Click += new System.EventHandler(this.TopMiddleButton_Click);
+            // 
+            // TopRightButton
+            // 
+            this.TopRightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.TopRightButton.Location = new System.Drawing.Point(914, 181);
+            this.TopRightButton.Name = "TopRightButton";
+            this.TopRightButton.Size = new System.Drawing.Size(165, 153);
+            this.TopRightButton.TabIndex = 3;
+            this.TopRightButton.UseVisualStyleBackColor = true;
+            this.TopRightButton.Click += new System.EventHandler(this.TopRightButton_Click);
+            // 
+            // MiddleLeftButton
+            // 
+            this.MiddleLeftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.MiddleLeftButton.Location = new System.Drawing.Point(572, 340);
+            this.MiddleLeftButton.Name = "MiddleLeftButton";
+            this.MiddleLeftButton.Size = new System.Drawing.Size(165, 153);
+            this.MiddleLeftButton.TabIndex = 4;
+            this.MiddleLeftButton.UseVisualStyleBackColor = true;
+            this.MiddleLeftButton.Click += new System.EventHandler(this.MiddleLeftButton_Click);
+            // 
+            // MiddleButton
+            // 
+            this.MiddleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.MiddleButton.Location = new System.Drawing.Point(743, 340);
+            this.MiddleButton.Name = "MiddleButton";
+            this.MiddleButton.Size = new System.Drawing.Size(165, 153);
+            this.MiddleButton.TabIndex = 5;
+            this.MiddleButton.UseVisualStyleBackColor = true;
+            this.MiddleButton.Click += new System.EventHandler(this.MiddleButton_Click);
+            // 
+            // MiddleRightButton
+            // 
+            this.MiddleRightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.MiddleRightButton.Location = new System.Drawing.Point(914, 340);
+            this.MiddleRightButton.Name = "MiddleRightButton";
+            this.MiddleRightButton.Size = new System.Drawing.Size(165, 153);
+            this.MiddleRightButton.TabIndex = 6;
+            this.MiddleRightButton.UseVisualStyleBackColor = true;
+            this.MiddleRightButton.Click += new System.EventHandler(this.MiddleRightButton_Click);
+            // 
+            // BottomLeftButton
+            // 
+            this.BottomLeftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.BottomLeftButton.Location = new System.Drawing.Point(572, 499);
+            this.BottomLeftButton.Name = "BottomLeftButton";
+            this.BottomLeftButton.Size = new System.Drawing.Size(165, 153);
+            this.BottomLeftButton.TabIndex = 7;
+            this.BottomLeftButton.UseVisualStyleBackColor = true;
+            this.BottomLeftButton.Click += new System.EventHandler(this.BottomLeftButton_Click);
+            // 
+            // BottomMiddleButton
+            // 
+            this.BottomMiddleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.BottomMiddleButton.Location = new System.Drawing.Point(743, 499);
+            this.BottomMiddleButton.Name = "BottomMiddleButton";
+            this.BottomMiddleButton.Size = new System.Drawing.Size(165, 153);
+            this.BottomMiddleButton.TabIndex = 8;
+            this.BottomMiddleButton.UseVisualStyleBackColor = true;
+            this.BottomMiddleButton.Click += new System.EventHandler(this.BottomMiddleButton_Click);
+            // 
+            // BottomRightButton
+            // 
+            this.BottomRightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.BottomRightButton.Location = new System.Drawing.Point(914, 499);
+            this.BottomRightButton.Name = "BottomRightButton";
+            this.BottomRightButton.Size = new System.Drawing.Size(165, 153);
+            this.BottomRightButton.TabIndex = 9;
+            this.BottomRightButton.UseVisualStyleBackColor = true;
+            this.BottomRightButton.Click += new System.EventHandler(this.BottomRightButton_Click);
+            // 
+            // PlayAgainButton
+            // 
+            this.PlayAgainButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.PlayAgainButton.Location = new System.Drawing.Point(599, 658);
+            this.PlayAgainButton.Name = "PlayAgainButton";
+            this.PlayAgainButton.Size = new System.Drawing.Size(452, 96);
+            this.PlayAgainButton.TabIndex = 10;
+            this.PlayAgainButton.Text = "Lets Play Again";
+            this.PlayAgainButton.UseVisualStyleBackColor = true;
+            this.PlayAgainButton.Click += new System.EventHandler(this.PlayAgainButton_Click);
+            // 
+            // TurnLabel
+            // 
+            this.TurnLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TurnLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.TurnLabel.Location = new System.Drawing.Point(12, 353);
+            this.TurnLabel.Name = "TurnLabel";
+            this.TurnLabel.Size = new System.Drawing.Size(426, 64);
+            this.TurnLabel.TabIndex = 11;
+            this.TurnLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.DescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.DescriptionLabel.Location = new System.Drawing.Point(12, 290);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(423, 44);
+            this.DescriptionLabel.TabIndex = 12;
+            this.DescriptionLabel.Text = "Player Who\'s Up Next";
+            // 
+            // WinnerLabel
+            // 
+            this.WinnerLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.WinnerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.WinnerLabel.Location = new System.Drawing.Point(1187, 361);
+            this.WinnerLabel.Name = "WinnerLabel";
+            this.WinnerLabel.Size = new System.Drawing.Size(239, 64);
+            this.WinnerLabel.TabIndex = 13;
+            // 
+            // Description2Label
+            // 
+            this.Description2Label.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Description2Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Description2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Description2Label.Location = new System.Drawing.Point(1155, 290);
+            this.Description2Label.Name = "Description2Label";
+            this.Description2Label.Size = new System.Drawing.Size(290, 44);
+            this.Description2Label.TabIndex = 14;
+            this.Description2Label.Text = "Who Won?";
+            this.Description2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Activity13
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1494, 786);
+            this.Controls.Add(this.Description2Label);
+            this.Controls.Add(this.WinnerLabel);
+            this.Controls.Add(this.DescriptionLabel);
+            this.Controls.Add(this.TurnLabel);
+            this.Controls.Add(this.PlayAgainButton);
+            this.Controls.Add(this.BottomRightButton);
+            this.Controls.Add(this.BottomMiddleButton);
+            this.Controls.Add(this.BottomLeftButton);
+            this.Controls.Add(this.MiddleRightButton);
+            this.Controls.Add(this.MiddleButton);
+            this.Controls.Add(this.MiddleLeftButton);
+            this.Controls.Add(this.TopRightButton);
+            this.Controls.Add(this.TopMiddleButton);
+            this.Controls.Add(this.TopLeftButton);
+            this.Controls.Add(this.TitleLabel);
+            this.Name = "Activity13";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Activity13_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Button TopLeftButton;
+        private System.Windows.Forms.Button TopMiddleButton;
+        private System.Windows.Forms.Button TopRightButton;
+        private System.Windows.Forms.Button MiddleLeftButton;
+        private System.Windows.Forms.Button MiddleButton;
+        private System.Windows.Forms.Button MiddleRightButton;
+        private System.Windows.Forms.Button BottomLeftButton;
+        private System.Windows.Forms.Button BottomMiddleButton;
+        private System.Windows.Forms.Button BottomRightButton;
+        private System.Windows.Forms.Button PlayAgainButton;
+        private System.Windows.Forms.Label TurnLabel;
+        private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.Label WinnerLabel;
+        private System.Windows.Forms.Label Description2Label;
+    }
+}
+
---
+++ Activity 13/Activity 13/Form1.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_13
+
+    //CST 150
+    //Chaz Pollino
+    // 6/9/23
+    // This is my work
+{
+    public partial class Activity13 : Form
+    {
+        public Activity13()
+        {
+            InitializeComponent();
+            TurnLabel.Text = "X";
+
+        }
+        bool IsXTurn = true;
+        String winner = "";
+
+        String[,] gameBoard = new string[3, 3];
+
+       void TurnLabelUpdate()
+        {
+            if (IsXTurn)
+            {
+                TurnLabel.Text = "X";
+            }
+            else
+            {
+                TurnLabel.Text = "O";
+            }
+
+        }
+        bool DidSomeoneWin()
+        {
+            if (gameBoard[0, 0] =="X" && gameBoard[0,1] == "X" && gameBoard[0,2] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+            if (gameBoard[0, 0] == "O" && gameBoard[0, 1] == "O" && gameBoard[0, 2] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            
+            if (gameBoard[1, 0] == "X" && gameBoard[1, 1] == "X" && gameBoard[1, 2] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (gameBoard[1, 0] == "O" && gameBoard[1, 1] == "O" && gameBoard[1, 2] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            
+            if (gameBoard[2, 0] == "X" && gameBoard[2, 1] == "X" && gameBoard[2, 2] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (gameBoard[2, 0] == "O" && gameBoard[2, 1] == "O" && gameBoard[2, 2] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            
+            if (gameBoard[0, 0] == "X" && gameBoard[1, 0] == "X" && gameBoard[2, 0] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (gameBoard[0, 0] == "O" && gameBoard[1, 0] == "O" && gameBoard[2, 0] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            
+            if (gameBoard[0, 1] == "X" && gameBoard[1, 1] == "X" && gameBoard[2, 1] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (gameBoard[0, 1] == "O" && gameBoard[1, 1] == "O" && gameBoard[2, 1] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            
+            if (gameBoard[0, 2] == "X" && gameBoard[1, 2] == "X" && gameBoard[2, 2] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (gameBoard[0, 2] == "O" && gameBoard[1, 2] == "O" && gameBoard[2, 2] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            
+            if (gameBoard[0, 0] == "X" && gameBoard[1, 1] == "X" && gameBoard[2, 2] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (gameBoard[0, 0] == "O" && gameBoard[1, 1] == "O" && gameBoard[2, 2] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            
+            if (gameBoard[0, 2] == "X" && gameBoard[1, 1] == "X" && gameBoard[2, 0] == "X")
+            {
+                winner = "X";
+                return true;
+            }
+
+            if (gameBoard[0, 2] == "O" && gameBoard[1, 1] == "O" && gameBoard[2, 0] == "O")
+            {
+                winner = "O";
+                return true;
+            }
+
+            return false;
+        }
+
+        private void Activity13_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TopLeftButton_Click(object sender, EventArgs e)
+        {
+            if (IsXTurn)
+            {
+                TopLeftButton.Text = "X";
+                gameBoard[0, 0] = "X";
+                IsXTurn = false;
+                TurnLabelUpdate();
+            }
+            else
+            {
+                TopLeftButton.Text = "O";
+                gameBoard[0, 0] = "O";
+                IsXTurn = true;
+                TurnLabelUpdate();
+            }
+
+            bool won = DidSomeoneWin();
+            if (won)
+            {
+                WinnerLabel.Text = (winner + " Won!!!!!!");
+            }
+        }
+
+        private void TopMiddleButton_Click(object sender, EventArgs e)
+        {
+            if (IsXTurn)
+            {
+                TopMiddleButton.Text = "X";
+                gameBoard[0, 1] = "X";
+                IsXTurn = false;
+                TurnLabelUpdate();
+            }
+            else
+            {
+                TopMiddleButton.Text = "O";
+                gameBoard[0, 1] = "O";
+                IsXTurn = true;
+                TurnLabelUpdate();
+            }
+
+            bool won = DidSomeoneWin();
+            if (won)
+            {
+                WinnerLabel.Text = (winner + " Won!!!!!!");
+            }
+        }
+
+        private void TopRightButton_Click(object sender, EventArgs e)
+        {
+            if (IsXTurn)
+            {
+                TopRightButton.Text = "X";
+                gameBoard[0, 2] = "X";
+                IsXTurn = false;
+                TurnLabelUpdate();
+            }
+            else
+            {
+                TopRightButton.Text = "O";
+                gameBoard[0, 2] = "O";
+                IsXTurn = true;
+                TurnLabelUpdate();
+            }
+
+            bool won = DidSomeoneWin();
+            if (won)
+            {
+                WinnerLabel.Text = (winner + " Won!!!!!!");
+            }
+        }
+
+        private void MiddleLeftButton_Click(object sender, EventArgs e)
+        {
+            if (IsXTurn)
+            {
+                MiddleLeftButton.Text = "X";
+                gameBoard[1, 0] = "X";
+                IsXTurn = false;
+                TurnLabelUpdate();
+            }
+            else
+            {
+                MiddleLeftButton.Text = "O";
+                gameBoard[1, 0] = "O";
+                IsXTurn = true;
+                TurnLabelUpdate();
+            }
+
+            bool won = DidSomeoneWin();
+            if (won)
+            {
+                WinnerLabel.Text = (winner + " Won!!!!!!");
+            }
+        }
+
+        private void MiddleButton_Click(object sender, EventArgs e)
+        {
+            if (IsXTurn)
+            {
+                MiddleButton.Text = "X";
+                gameBoard[1, 1] = "X";
+                IsXTurn = false;
+                TurnLabelUpdate();
+            }
+            else
+            {
+                MiddleButton.Text = "O";
+                gameBoard[1, 1] = "O";
+                IsXTurn = true;
+                TurnLabelUpdate();
+            }
+
+            bool won = DidSomeoneWin();
+            if (won)
+            {
+                WinnerLabel.Text = (winner + " Won!!!!!!");
+            }
+        }
+
+        private void MiddleRightButton_Click(object sender, EventArgs e)
+        {
+            if (IsXTurn)
+            {
+                MiddleRightButton.Text = "X";
+                gameBoard[1, 2] = "X";
+                IsXTurn = false;
+                TurnLabelUpdate();
+            }
+            else
+            {
+                MiddleRightButton.Text = "O";
+                gameBoard[1, 2] = "O";
+                IsXTurn = true;
+                TurnLabelUpdate();
+            }
+
+            bool won = DidSomeoneWin();
+            if (won)
+            {
+                WinnerLabel.Text = (winner + " Won!!!!!!");
+            }
+        }
+
+        private void BottomLeftButton_Click(object sender, EventArgs e)
+        {
+            if (IsXTurn)
+            {
+                BottomLeftButton.Text = "X";
+                gameBoard[2, 0] = "X";
+                IsXTurn = false;
+                TurnLabelUpdate();
+            }
+            else
+            {
+                BottomLeftButton.Text = "O";
+                gameBoard[2, 0] = "O";
+                IsXTurn = true;
+                TurnLabelUpdate();
+            }
+
+            bool won = DidSomeoneWin();
+            if (won)
+            {
+                WinnerLabel.Text = (winner + " Won!!!!!!");
+            }
+        }
+
+        private void BottomMiddleButton_Click(object sender, EventArgs e)
+        {
+            if (IsXTurn)
+            {
+                BottomMiddleButton.Text = "X";
+                gameBoard[2, 1] = "X";
+                IsXTurn = false;
+                TurnLabelUpdate();
+            }
+            else
+            {
+                BottomMiddleButton.Text = "O";
+                gameBoard[2, 1] = "O";
+                IsXTurn = true;
+                TurnLabelUpdate();
+            }
+
+            bool won = DidSomeoneWin();
+            if (won)
+            {
+                WinnerLabel.Text = (winner + " Won!!!!!!");
+            }
+        }
+
+        private void BottomRightButton_Click(object sender, EventArgs e)
+        {
+            if (IsXTurn)
+            {
+                BottomRightButton.Text = "X";
+                gameBoard[2, 2] = "X";
+                IsXTurn = false;
+                TurnLabelUpdate();
+            }
+            else
+            {
+                BottomRightButton.Text = "O";
+                gameBoard[2, 2] = "O";
+                IsXTurn = true;
+                TurnLabelUpdate();
+            }
+
+            bool won = DidSomeoneWin();
+            if (won)
+            {
+                WinnerLabel.Text = (winner + " Won!!!!!!");
+            }
+        }
+
+        private void PlayAgainButton_Click(object sender, EventArgs e)
+        {
+            TopLeftButton.Text = "";
+            TopMiddleButton.Text = "";
+            TopRightButton.Text = "";
+            MiddleLeftButton.Text = "";
+            MiddleButton.Text = "";
+            MiddleRightButton.Text = "";
+            BottomLeftButton.Text = "";
+            BottomMiddleButton.Text = "";
+            BottomRightButton.Text = "";
+            WinnerLabel.Text = "";
+
+            Array.Clear(gameBoard,0,gameBoard.Length);
+        }
+    }
+}
